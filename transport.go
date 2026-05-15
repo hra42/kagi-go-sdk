b90a0e42a37dbd19ee0a71d14226402f942b4517
Henry Rausch <green.pool7936@hra42.com>
2026-05-15 02:44:47 +0200

    Implement request transport layer

---
+++ transport.go
@@ -0,0 +1,68 @@
+package kagi
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"errors"
+	"io"
+	"net/http"
+	"net/url"
+	"strings"
+)
+
+const authorizationScheme = "Bot"
+
+func (c *Client) newRequest(ctx context.Context, method, endpointPath string, query url.Values, body any) (*http.Request, error) {
+	if ctx == nil {
+		return nil, errors.New("kagi: nil context")
+	}
+
+	var requestBody io.Reader
+	if body != nil {
+		var buf bytes.Buffer
+		if err := json.NewEncoder(&buf).Encode(body); err != nil {
+			return nil, err
+		}
+		requestBody = &buf
+	}
+
+	req, err := http.NewRequestWithContext(ctx, method, c.endpointURL(endpointPath, query), requestBody)
+	if err != nil {
+		return nil, err
+	}
+
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("Authorization", authorizationScheme+" "+c.apiKey)
+	req.Header.Set("User-Agent", c.userAgent)
+	if body != nil {
+		req.Header.Set("Content-Type", "application/json")
+	}
+
+	return req, nil
+}
+
+func (c *Client) do(req *http.Request) (*http.Response, error) {
+	return c.httpClient.Do(req)
+}
+
+func (c *Client) endpointURL(endpointPath string, query url.Values) string {
+	endpoint := *c.baseURL
+
+	basePath := strings.TrimRight(endpoint.Path, "/")
+	endpointPath = strings.TrimLeft(endpointPath, "/")
+	switch {
+	case endpointPath == "":
+		endpoint.Path = basePath
+	case basePath == "":
+		endpoint.Path = "/" + endpointPath
+	default:
+		endpoint.Path = basePath + "/" + endpointPath
+	}
+
+	if len(query) > 0 {
+		endpoint.RawQuery = query.Encode()
+	}
+
+	return endpoint.String()
+}
